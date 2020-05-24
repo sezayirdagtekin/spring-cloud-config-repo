cef4564d389ed339b1c31e179a4fa420da1f2f17
sezayirdagtekin <sezayir@hotmail.com>
2020-05-24 08:58:38 +0300

    add refresh scope

--- springboot-config-client/src/main/java/com/sezayri/springbootconfig/controller/GreetingController.java
+++ springboot-config-client/src/main/java/com/sezayri/springbootconfig/controller/GreetingController.java
@@ -5,11 +5,13 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class GreetingController {
 
 	@Autowired
